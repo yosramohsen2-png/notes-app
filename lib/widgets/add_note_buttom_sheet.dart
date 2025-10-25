1c1a90a9ed2839ed2c9e4578d92d55be4b26ef08
Yosra Mohsen <yosramohsen2@gmail.com>
2025-10-25 11:49:57 +0300

    add field to add button field

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -8,7 +8,14 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(children: [SizedBox(height: 32), CustomTextField()]),
+      child: Column(
+        children: const [
+          SizedBox(height: 32),
+          CustomTextField(hint: 'title'),
+          SizedBox(height: 16),
+          CustomTextField(hint: 'content', maxLines: 5),
+        ],
+      ),
     );
   }
 }
