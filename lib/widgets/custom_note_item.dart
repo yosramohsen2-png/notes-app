cf1b909cda11f002b46893d7099858a629ee4bdf
Yosra Mohsen <yosramohsen2@gmail.com>
2025-10-25 15:57:48 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
@@ -7,46 +8,58 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 26),
-              ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return EditNoteView();
+              },
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  'Flutter Tips',
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
 
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    'Build Your Career With Yosra',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete),
+                  color: const Color.fromARGB(255, 50, 50, 50),
+                  iconSize: 24,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 24, top: 16),
                 child: Text(
-                  'Build Your Career With Yosra',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
+                  'May 21,2025',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 49, 48, 48),
+                    fontSize: 14,
+                  ),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.delete),
-                color: const Color.fromARGB(255, 50, 50, 50),
-                iconSize: 24,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24, top: 16),
-              child: Text(
-                'May 21,2025',
-                style: TextStyle(
-                  color: const Color.fromARGB(255, 49, 48, 48),
-                  fontSize: 14,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
