0daaeca7fe486e3d0dfb6a00a18850cec0ea476c
Yosra Mohsen <yosramohsen2@gmail.com>
2025-10-27 12:16:04 +0300

    add note state and add note method to add note cubit

--- lib/add_note_cubit/cubit/add_note_state.dart
+++ lib/add_note_cubit/cubit/add_note_state.dart
@@ -4,3 +4,15 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+// ✅ تم التعديل
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+// ✅ تم التعديل
+final class AddNoteFaillure extends AddNoteState {
+  final String error;
+
+  AddNoteFaillure({required this.error}); // يفضل إضافة const
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note/costants.dart';
+import 'package:note/models/note_model.dart';
 import 'package:note/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(NoteApp());
 }
 
